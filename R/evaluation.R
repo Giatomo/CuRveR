a3feb0de5ebd5b148d08435cde06c9b0cf746c3c
Thomas <thomas.giatomo.lamotoutlook.com>
2022-03-09 11:12:01 +0100

    test Vs code

--- R/evaluation.R
+++ R/evaluation.R
@@ -20,7 +20,7 @@ mse <- function(values, fitted_values) {
 
 # Mean Square Reduced Error
 msre <- function(values, fitted_values) {
-  mse(values, fitted_values)/(sd(value)^2)
+  mse(values, fitted_values) / (sd(values)^2)
 }
 
 # Root Mean Square Error
@@ -30,41 +30,42 @@ rmsq <- function(values, fitted_values) {
 
 # Relative Root Mean Square Error
 rrmsq <- function(values, fitted_values) {
-  100 * rmsq(values, fitted_values)/mean(values)
+  100 * rmsq(values, fitted_values) / mean(values)
 }
 
 # Relative Mean Absolute Error
 rmae <- function(values, fitted_values) {
-  100 * mae(values, fitted_values)/mean(values)
+  100 * mae(values, fitted_values) / mean(values)
 }
 
 # Standardised Root Mean Square Error
 srmsq <- function(values, fitted_values) {
-  rmsq(values, fitted_values)/sd(values)
+  rmsq(values, fitted_values) / sd(values)
 }
 
-mean_deviation <- function(values){
+mean_deviation <- function(values) {
   values - mean(values)
 }
 
-variance_explained <- function(values, fitted_values){
-  100 * (1 - (sum(square_error(values, fitted_values))/sum(mean_deviation(values)^2)))
+variance_explained <- function(values, fitted_values) {
+  100 * (1 - (sum(square_error(values, fitted_values)) / sum(mean_deviation(values)^2)))
 }
 
-legate_mccabe <- function(values, fitted_values){
-  100 * (1 - (sum(absolute_error(values, fitted_values))/sum(abs(mean_deviation(values)))))
+
+legate_mccabe <- function(values, fitted_values) {
+  100 * (1 - (sum(absolute_error(values, fitted_values)) / sum(abs(mean_deviation(values)))))
 }
 
-willmott_index_agreement <- function(values, fitted_values){
+willmott_index_agreement <- function(values, fitted_values) {
   e1 <- legate_mccabe(values, fitted_values)
-  if (e1 < 0){
-    return(100 * ((sum(abs(mean_deviation(values)))/sum(absolute_error(values, fitted_values))) - 1))
+  if (e1 < 0) {
+    return(100 * ((sum(abs(mean_deviation(values))) / sum(absolute_error(values, fitted_values))) - 1))
   }
   else {
     return(e1)
   }
 }
 
-pearson_product_moment_r <- function(values, fitted_values){
+pearson_product_moment_r <- function(values, fitted_values) {
   sum(mean_deviation(values) * mean_deviation(fitted_values)) / sqrt(sum((mean_deviation(values)^2) * (mean_deviation(fitted_values)^2)))
 }
--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 ## Overview
 CuRveR was originally created to provide an easy to use analysis tool for killing curves assay done in microplates with _Bdellovibrio bacteriovorus_ ([Remy, Lamot and Santin _et al_. (2022)](https://www.sciencedirect.com/science/article/pii/S2666166721008108)). Due to its flexible implementation, it can be used to analyse a broad range of other assays requiring to fit an equation to the data like growth curves or β-galactosidase assays.
 ## Installation
-Make sure you have R ≥ 4.1.0 installed on your device (https://www.r-project.org/) with the [devtools package](https://github.com/r-lib/devtools)
-Run the following command in the R console
+Make sure you have R ≥ 4.1.0 installed on your device (https://www.r-project.org/) with the [devtools package](https://github.com/r-lib/devtools)  
+Run the following command in the R console :
 
 ``` r
 # Download the latest version of CuRveR
 devtools::install_github("Giatomo/curver")
 ```
 ## Usage
+To open CuRveR, run the following command in the R console : 
+``` r
+# Launch the GUI application
+CuRveR::run_curver()
+```
