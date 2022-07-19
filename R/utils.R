57babc4d77c105e76c28a7fae9a43ea0aa9606c5
Giatomo <48335512+Giatomo@users.noreply.github.com>
2022-07-19 16:11:12 +0200

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -62,16 +62,16 @@ is_excel_file <- function(file) {
 
 
 #' @export
-fit_data <- function(.data, .groups, .value, .time, model = RichardModel, optimizer = DeOptimizer, loss_fun = least_absolute_deviation, min_bound_perc = 0.10, max_bound_perc = 0.10, r_bound_tresh = 100, n_values_for_estimate = 1) {
+fit_data <- function(.data, .groups, .value, .time, .id = NULL,  model = RichardModel, optimizer = DeOptimizer, loss_fun = least_absolute_deviation, min_bound_perc = 0.10, max_bound_perc = 0.10, r_bound_tresh = 100, n_values_for_estimate = 1) {
   optimizer <- optimizer$new(loss_fun)
 
   .data |>
-    select({{ .groups }}, {{ .value }}, {{ .time }}, well) |>
+    select({{ .groups }}, {{ .value }}, {{ .time }}, {{.id}}) |>
     group_by(across(c({{ .groups }}))) |>
     nest(
       {{ .time }} := {{ .time }},
       {{ .value }} := {{ .value }},
-      well = well
+      {{.id}} = {{.id}}
     ) |>
     rowwise() |>
     mutate(model = list(model$new({{ .time  }}, {{ .value }}, min_bound_perc = min_bound_perc, max_bound_perc = max_bound_perc, r_bound_tresh = r_bound_tresh, n_values_for_estimate = n_values_for_estimate))) |>
